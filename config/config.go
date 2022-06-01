4b107ba1db609fbc6bf2ba8e13b6f808cb09d23c
Kadrick <kbk2581553@gmail.com>
2022-06-01 13:42:37 +0900

    dev : configuration init

---
+++ config/config.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+var conf = Config{}
+
+type Config struct {
+	Local Local `yaml:"Local"`
+}
+
+type Local struct {
+	Port int `yaml:"Port"`
+}
+
+func InitConfig() {
+	var err error
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./config/")
+
+	if err = viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			panic(fmt.Errorf("No such config file: %s \n", "config.yaml"))
+		} else {
+			panic(fmt.Errorf("Fatal error config file: %w \n", err))
+		}
+	}
+
+	err = viper.Unmarshal(&conf)
+	if err != nil {
+		panic(fmt.Errorf("Can not read yaml file \n"))
+	}
+
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Config file changed:", e.Name)
+
+		if err = viper.ReadInConfig(); err != nil {
+			if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+				panic(fmt.Errorf("No such config file: %s \n", "config.yaml"))
+			} else {
+				panic(fmt.Errorf("Fatal error config file: %w \n", err))
+			}
+		}
+
+		err = viper.Unmarshal(&conf)
+		if err != nil {
+			panic(fmt.Errorf("Can not read yaml file \n"))
+		}
+	})
+}
+
+func GetConfig() Config {
+	return conf
+}
--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+	"todo/config"
 )
+
+func init() {
+	config.InitConfig()
+}
 
 func main() {
 	fmt.Println("hello world")
+	port := config.GetConfig().Local.Port
+	fmt.Println(port)
 	return
 }
