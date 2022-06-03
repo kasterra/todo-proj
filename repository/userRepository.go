fc083e99e332919fa8c2142d4cdd616c73a949aa
Kadrick <kbk2581553@gmail.com>
2022-06-03 20:39:35 +0900

    dev : update root dir #2

--- repository/userRepository.go
+++
@@ -1,7 +0,0 @@
-package repository
-
-import "todo/model"
-
-type UserRepository interface {
-	Save(user *model.User) (*model.User, error)
-}
--- service/userService.go
+++
@@ -1,9 +0,0 @@
-package service
-
-import (
-	"todo/model/dto"
-)
-
-type UserService interface {
-	SignUp(userDto *dto.UserDto) (*dto.UserDto, error)
-}
