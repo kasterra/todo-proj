45ea3fca12700624ed46c1856c891eef79e5a7c2
kasterra <kasterra@naver.com>
2022-06-03 23:23:01 +0900

    ✨ feat: styling & Home page publish

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-import { css, Global } from "@emotion/react";
-import normalize from "emotion-normalize";
-import Routes from "./pages/Routes";
-import { appTheme } from "./themes/appTheme";
-
-const theme = appTheme;
+import React from 'react';
+import { css, Global } from '@emotion/react';
+import normalize from 'emotion-normalize';
+import Routes from './pages/Routes';
+import { theme as chakraTheme } from '@chakra-ui/react';
 
 const App = () => {
   return (
@@ -14,19 +12,31 @@ const App = () => {
           ${normalize}
           * {
             box-sizing: border-box;
+            background-color: transparent;
+            border: none;
           }
           a {
             text-decoration: none;
             color: inherit;
           }
-          body{
-            background-color: ${theme.bgColor};
+          body {
+            background-color: #f5f5f5;
+            font: ${chakraTheme.fonts.body};
+          }
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6 {
+            margin: 0;
+            font: ${chakraTheme.fonts.heading};
           }
         `}
       />
       <Routes />
     </>
   );
-}
+};
 
 export default App;
