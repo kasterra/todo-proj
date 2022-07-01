88fcc1fe20e3453107209c8b1b89799223ad6e3c
kasterra <kasterra@naver.com>
2022-07-02 08:37:33 +0900

    temp : Table component temp fix

--- frontend/src/components/Table.tsx
+++ frontend/src/components/Table.tsx
@@ -60,32 +60,37 @@ interface ITableProps {
     headList: { width: string | number; content: any }[];
     height: string | number;
   };
-  contentTable: { content: any[][]; height: string | number };
+  contentTable: { content: any[][] | undefined; height: string | number }; // FIXME: 임시로 undefined로 땜질했는데, 나중에 이것도 보수 필요
 }
 
 const Table = ({ headInfo, contentTable }: ITableProps) => {
-  const renderItem = useCallback((renderer: any) => {
+  const renderItem = useCallback((renderer: any, key: any) => {
     if (typeof renderer === 'function') {
       return renderer();
-    } else if (typeof renderer === 'string' || typeof renderer === 'object') {
+    } else if (typeof renderer === 'object') {
       return renderer;
+    } else if (typeof renderer === 'string') {
+      <span key={key}>{renderer}</span>;
     }
   }, []);
-  return (
-    <Container>
-      <TableHeader headInfo={headInfo}>
-        {headInfo.headList.map(item => renderItem(item.content))}
-      </TableHeader>
+  if (!contentTable.content)
+    return null; // FIXME: 이것 역시 땜질한 부분. FIX가 필요 이하 return 문 까지 다
+  else
+    return (
+      <Container>
+        <TableHeader headInfo={headInfo}>
+          {headInfo.headList.map((item, idx) => renderItem(item.content, idx))}
+        </TableHeader>
 
-      {contentTable.content.map(row => (
-        <TableRow headInfo={headInfo} height={contentTable.height}>
-          {row.map(item => (
-            <TableCell>{renderItem(item)}</TableCell>
-          ))}
-        </TableRow>
-      ))}
-    </Container>
-  );
+        {contentTable.content.map((row, idx) => (
+          <TableRow headInfo={headInfo} height={contentTable.height}>
+            {row.map(item => (
+              <TableCell>{renderItem(item, idx)}</TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </Container>
+    );
 };
 
 export default Table;
